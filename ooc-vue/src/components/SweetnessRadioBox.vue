02d7e296fbdd827aa290b16307a34a56d43834ba
Sorawit Kongnurat <trainer.mapring@gmail.com>
2018-03-18 00:46:31 +0700

    Merge with Win's Checkout.vue

---
+++ ooc-vue/src/components/SweetnessRadioBox.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="btn-group btn-group-toggle" data-toggle="buttons">
+    <label class="btn btn-secondary active">
+      <input type="radio" name="options" id="option1" autocomplete="off" checked> Less
+    </label>
+    <label class="btn btn-secondary">
+      <input type="radio" name="options" id="option2" autocomplete="off"> Normal
+    </label>
+    <label class="btn btn-secondary">
+      <input type="radio" name="options" id="option3" autocomplete="off"> Extra
+    </label>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "SweetnessRadioBox"
+  }
+</script>
+
+<style scoped>
+
+</style>
