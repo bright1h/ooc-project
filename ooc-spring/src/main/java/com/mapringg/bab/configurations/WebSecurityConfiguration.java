f10b9eab04382b70ba46fd4f1ca5de0943cfa256
tui95 <khemmatat.thean@gmail.com>
2018-03-21 19:22:16 +0700

    merge from master branch

--- ooc-spring/src/main/java/com/mapringg/bab/configurations/WebSecurityConfiguration.java
+++ ooc-spring/src/main/java/com/mapringg/bab/configurations/WebSecurityConfiguration.java
@@ -12,6 +12,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http
+
     }
 }
