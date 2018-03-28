c963a77fdaac1d5b788667e1cfd0c1b745d8e890
tui95 <khemmatat.thean@gmail.com>
2018-03-28 14:59:51 +0700

    can communicate between frontend and backend

--- ooc-spring/src/main/java/com/mapringg/bab/configurations/WebSecurityConfiguration.java
+++ ooc-spring/src/main/java/com/mapringg/bab/configurations/WebSecurityConfiguration.java
@@ -1,21 +1,21 @@
-package com.mapringg.bab.configurations;
-
-import org.springframework.context.annotation.Configuration;
+//package com.mapringg.bab.configurations;
+//
+//import org.springframework.context.annotation.Configuration;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 //import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+//
 //@Configuration
 //@EnableWebSecurity
-public class WebSecurityConfiguration {
-
-
+//public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+//
+//
 //    @Override
 //    protected void configure(HttpSecurity http) throws Exception {
 //        http
+//            .cors().and()
 //            .authorizeRequests()
 //                .antMatchers("/api/*").permitAll();
-////                .antMatchers("/api/customers").authenticated();
 //
 //    }
-}
+//}
