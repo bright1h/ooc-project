a37c091d38046f9d27e3ec15a7b2f58339474a10
Aphisit Areechitsakul <bright40081@gmail.com>
2018-04-05 18:31:49 +0700

    fix ui and change OrderHistory ui

--- ooc-vue/src/views/OrderHistory.vue
+++ ooc-vue/src/views/OrderHistory.vue
@@ -1,90 +1,92 @@
 <template>
-  <div class="order-history">
-    <!-- <Header/> -->
+  <div class="order-list p-2 rounded"
+  style="
+    background-color: #ffffff;">
     <div class="container">
       <div class="row border-bottom py-1">
         <h1 class="text-dark ">Order History</h1>
       </div>
       <div class="row my-4">
-        <AdminSideBar/>
         <div class="col">
-          <form class="form-inline">
-            <div class="form-group mx-sm-3 mb-2">
-              <label for="inputDate" class="sr-only">Date</label>
-              <input type="date" class="form-control" id="inputDate">
+          <div id="accordion">
+            <!-- add this later v-bind:key="index" v-for="(order,index) in orderlist" -->
+              <div class="card"  >
+                <div class="card-header alert-success" id="headingOne">
+                  <!-- Change collapse one to other name otherwise it's not gonna toggle -->
+                  <h6 class="row mb-0 font-weight-bold" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    
+                    <div class="col-sm-4 m-auto">
+                    
+                      <span class="">OrderID # 1</span>
+                    
+                    </div>
+                    <div class="col-sm-4 m-auto">
+                      <span>  
+                        By FirstName, LastName
+                      </span>
+                    
+                    </div>
+                    <div class="col-sm-4 m-auto">
+                      xx/xx/xxxx
+                    </div>
+                    
+                  </h6>
+                </div>
+
+                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                  <div class="card-body text-left">
+                    <u>Order: </u>
+                    <ul>
+                      <li v-bind:key="index" v-for="(data,index) in order">
+                        <div class="row">
+                          <div class="col">
+                            {{data.name}}
+                          </div>
+                          <div class="col">
+                            {{data.quantity}}
+                          </div>
+                        </div>
+                      </li>
+                      <li>1</li>
+                      <li>2</li>
+                      <li>3</li>
+                      <li>4</li>
+                    </ul>
+                    <u>Special Request: </u>
+                    <ul>
+                      <!-- <li>{{data.specialRequest}}</li> -->
+                      <li>test</li>
+                    </ul>
+                    <div class="alert alert-danger text-right">
+                      <span class="font-weight-bold"> Price : xx</span>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+              
             </div>
-            <button type="submit" class="btn btn-primary mb-2">Search</button>
-          </form>
-          <table class="table">
-            <thead>
-            <tr>
-              <th>First Name</th>
-              <th>Date</th>
-              <th>Menu</th>
-              <th>Quantity</th>
-              <th>Price</th>
-              <th>From</th>
-              <th>Till</th>
-              <th>Status</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-              <td>Sorawit</td>
-              <td>2018-02-20</td>
-              <td>Blueberry Cheesecake</td>
-              <td>1</td>
-              <td>100</td>
-              <td>2018-02-20</td>
-              <td>2018-02-22</td>
-              <td>Done</td>
-            </tr>
-            </tbody>
-          </table>
+          </div>
         </div>
-      </div>
     </div>
-    <!-- <Footer/> -->
   </div>
 </template>
 
+
 <script>
-  import AdminSideBar from '../components/AdminSideBar'
-  import Header from "../components/Header";
-  import Footer from "../components/Footer";
-
-  function getToday() {
-    let today = new Date();
-    let dd = today.getDate();
-    let mm = today.getMonth() + 1; //January is 0!
-    const yyyy = today.getFullYear();
-
-    if(dd<10) {
-      dd = '0'+dd
-    }
-
-    if(mm<10) {
-      mm = '0'+mm
-    }
-
-    today = yyyy + '-' + mm + '-' + dd;
-    return today;
-  }
-
   export default {
     name: 'OrderHistory',
-    components: {Footer, Header, AdminSideBar},
-    data() {
-      return {
-        today: ''
-      }
-    },
-    created() {
-      this.today = getToday();
-    }
   }
 </script>
 
 <style scoped>
+  .card-body{
 
+    background-color: #e9ebee ;
+  }
+
+  .alert{
+    font-size: 15px;
+
+  }
 </style>
